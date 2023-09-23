8bc63845538bc8e415900dec372d2f43db36136c
Lara Ramsey <lararams3y@gmail.com>
2023-09-23 14:16:05 -0700

    playin with JS

---
+++ script.js
@@ -0,0 +1,28 @@
+//'use strict';
+
+//console.log("Hello Piggy Fan!");
+//alert("Not a Piggy Fan! ? Click on Partner Link to learn about Dog Hairstyles!");
+
+//const usersName = prompt("Tell us your name, so we can personalize your experience here! **Special message for anyone named Piggy Fan!");
+//console.log(usersName);
+
+//let userEmail = prompt("Want weekly Piggy Updates? Send us your email!");
+//console.log(userEmail)
+
+//document.write("Hi, " + usersName + ". Welcome to my page!");
+
+if (usersName == "Piggy Fan!"){
+    alert("Wheek!Wheek!Wheek!");
+} else if (usersName == "Zia"){
+    alert("Wheek!Wheek!Wheek!");
+} else if (usersName == "Justus"){
+    alert("Wheek!Wheek!Wheek!");     
+} else if (usersName == "Kassie"){
+    alert("Wheek.");
+} else if (usersName == "Alyson"){
+    alert("Wheek.");
+} else if (usersName == "Andrew"){
+    alert("Wheek.");
+} else {
+    alert("Welcome! Can't wait to transform you into a Piggy Fan!");
+}
