fa97493b8ad10b3a21b44706d4a562937ac30de7
Lara Ramsey <lararams3y@gmail.com>
2023-09-24 15:55:21 -0700

    Shifting everything to JS Functions

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
 //'use strict';
 
-//console.log("Hello Piggy Fan!");
-//alert("Not a Piggy Fan! ? Click on Partner Link to learn about Dog Hairstyles!");
+console.log("Hello Piggy Fan!");
+alert("You've found the hub for Piggy Hairstyles! Not a Piggy Fan! ? Enter and click on Partner Link to learn about Dog Hairstyles!");
 
-//const usersName = prompt("Tell us your name, so we can personalize your experience here! **Special message for anyone named Piggy Fan!");
-//console.log(usersName);
+function getName(){
+    const usersName = prompt("Tell us your name so we can personalize your experience! **Special message for anyone named Piggy Fan!");
+    return usersName;
+}
 
-//let userEmail = prompt("Want weekly Piggy Updates? Send us your email!");
-//console.log(userEmail)
+//const theirName = getName();
+//console.log(theirName);
 
-//document.write("Hi, " + usersName + ". Welcome to my page!");
+function getEmail(){
+    let userEmail = prompt("Want weekly Piggy Updates? Send us your email!");
+    console.log(userEmail);
+    return userEmail;
+}
 
+// const theirEmail = getEmail
+
+function greetUser(){
+    document.write("Hi " + theirName + ", welcome to the wonderful world of Piggy Hairstyles!");
+}
+
+function specialMessage(usersName){
 if (usersName == "Piggy Fan!"){
-    alert("Wheek!Wheek!Wheek!");
+    document.write("Wheek!Wheek!Wheek! You dah best hooman!");
 } else if (usersName == "Zia"){
-    alert("Wheek!Wheek!Wheek!");
+    document.write("Wheek. Come on, Z, are you sure you aren't a Piggy Fan! ?");
 } else if (usersName == "Justus"){
-    alert("Wheek!Wheek!Wheek!");     
+    document.write("Wheek. Come on, J, are you sure you aren't a Piggy Fan! ?");     
 } else if (usersName == "Kassie"){
-    alert("Wheek.");
+    document.write("Wheek. Come on, Kassie, are you sure you aren't a Piggy Fan! ? Hint, hint!");
 } else if (usersName == "Alyson"){
-    alert("Wheek.");
+    document.write("Wheek. Come on, Alyson, are you sure you aren't a Piggy Fan! ? Hint, hint!");
 } else if (usersName == "Andrew"){
-    alert("Wheek.");
+    document.write("Wheek. Come on, Andrew, are you sure you aren't a Piggy Fan! ?");
 } else {
-    alert("Welcome! Can't wait to transform you into a Piggy Fan!");
+    document.write("Yay! Can't wait to transform you into a Piggy Fan!");
+}
+
+}
+
+function thankYouMessage (){
+    document.write("Thank you for visiting us, " + theirName + ". We'll send your Piggy News to " + theirEmail);
 }
