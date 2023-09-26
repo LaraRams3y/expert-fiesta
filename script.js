f942f51842f34f359cf5a7a86af9fa98d847bcd6
Lara Ramsey <lararams3y@gmail.com>
2023-09-25 19:43:31 -0700

    pushing to track photo file

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
 //'use strict';
 
 console.log("Hello Piggy Fan!");
-alert("You've found the hub for Piggy Hairstyles! Not a Piggy Fan! ? Enter anyway and click on Partner Link to learn about Dog Hairstyles!");
+//alert("You've found the hub for Piggy Hairstyles! Not a Piggy Fan! ? Enter anyway and click on Partner Link to learn about Dog Hairstyles!");
+const theirName = getName();
+let theirEmail = getEmail();
 
-function getName(){
-    const usersName = prompt("Tell us your name so we can personalize your experience! **Special message for anyone named Piggy Fan!");
+function getName() {
+    const usersName = prompt("Hiya! Tell us your name so we can personalize your experience!");
+    console.log(usersName);
     return usersName;
 }
 
 //const theirName = getName();
 //console.log(theirName);
 
-function getEmail(){
-    let userEmail = prompt("Want weekly Piggy Updates? Send us your email!");
+function getEmail() {
+    const userEmail = prompt("Want wheekly Piggy Updates? Give us your email!");
     console.log(userEmail);
     return userEmail;
 }
 
 // const theirEmail = getEmail
 
-function greetUser(){
+function greetUser() {
     document.write("Hi " + theirName + ", welcome to the wonderful world of Piggy Hairstyles!");
 }
 
-function specialMessage(usersName){
-if (usersName == "Piggy Fan!"){
-    document.write("Wheek!Wheek!Wheek! You dah best hooman!");
-} else if (usersName == "Zia"){
-    document.write("Wheek. Come on, Z, are you sure you aren't a Piggy Fan! ?");
-} else if (usersName == "Justus"){
-    document.write("Wheek. Come on, J, are you sure you aren't a Piggy Fan! ?");     
-} else if (usersName == "Kassie"){
-    document.write("Wheek. Come on, Kassie, are you sure you aren't a Piggy Fan! ? Hint, hint!");
-} else if (usersName == "Alyson"){
-    document.write("Wheek. Come on, Alyson, are you sure you aren't a Piggy Fan! ? Hint, hint!");
-} else if (usersName == "Andrew"){
-    document.write("Wheek. Come on, Andrew, are you sure you aren't a Piggy Fan! ?");
-} else {
-    document.write("Yay! Can't wait to transform you into a Piggy Fan!");
+function specialMessage(usersName) {
+    let required = 'Piggy Fan!';
+    while (usersName != required) {
+        usersName = prompt('Thnx for the email, but wrong name! LOL! We think ur name is actually "Piggy Fan!"? Try that name thing again.');
+    }
+    if (usersName == required) {
+        document.write('Wheek!Wheek!Wheek! You dah best hooman!');
+    }
 }
 
+//if (usersName == "Piggy Fan!"){
+//    document.write("Wheek!Wheek!Wheek! You dah best hooman!");
+//} else if (usersName == "Zia"){
+//    document.write("Wheek. Come on, Z, are you sure you aren't a Piggy Fan! ?");
+//} else if (usersName == "Justus"){
+//    document.write("Wheek. Come on, J, are you sure you aren't a Piggy Fan! ?");     
+//} else if (usersName == "Kassie"){
+//    document.write("Wheek. Come on, Kassie, are you sure you aren't a Piggy Fan! ? Hint, hint!");
+//} else if (usersName == "Alyson"){
+//    document.write("Wheek. Come on, Alyson, are you sure you aren't a Piggy Fan! ? Hint, hint!");
+//} else if (usersName == "Andrew"){
+//    document.write("Wheek. Come on, Andrew, are you sure you aren't a Piggy Fan! ?");
+//} else {
+//    document.write("Yay! Can't wait to transform you into a Piggy Fan!");
+//}
+
+
+function thankYouMessage() {
+    document.write("Thank you for visiting us, Piggy Fan! We'll send your Piggy News to " + theirEmail);
 }
 
-function thankYouMessage (){
-    document.write("Thank you for visiting us, " + theirName + ". We'll send your Piggy News to " + theirEmail);
+function rateMyPage(){
+    let rating = prompt("Hello Hooman. Even tho U haven't seen dis page yet, U really should give it 5 out of 5 wheeks. Pleeze rate our page 1 to 5.");
+    for (let i = 0; i < rating; i++){
+        document.write("<img class='loop-img' src:'Images/skinnypig.jpg' alt='cutest skinny pig, giving the hooman some side-eye'/>");
+    }
 }
